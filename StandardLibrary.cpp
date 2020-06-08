3736e417f946fe805fb79ff4adc03012895ac0c1
Suman <46301837+sumanpradhan07@users.noreply.github.com>
2020-06-08 08:17:35 +0530

    initial

---
+++ StandardLibrary.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
+//======================================
+
+#define ll long long
+#define for(i,n) for(i=0;i<n;i++)
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define PI 3.1415926535897932384626
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+//=========================================================
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+//==============================================================
+int main(){
+	
+	return 0;
+}
+
+//================================================================
+
